a03e3f24888e44a9f64358610e40c50255260119
VulpeOlesea <leseavulpe@gmail.com>
2024-10-05 11:49:01 +0100

    add modules

---
+++ modules/head.php
@@ -0,0 +1,32 @@
+<!--
+=====================================================================
+*   Projeto: Trabalho de Grupo
+*   UFCD: 5414 - Programação para a WEB - cliente (client-side)
+*   Turma: TPSI PL 1223
+*   Formador: Hugo Farinha
+*   Formandos: Leila Arruda, Lucas Patrício, Olesea Vulpe, Rúben Fonseca
+=====================================================================-->
+
+<?php 
+	include 'connections/config.php';
+	include 'functions/index.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>OnlineEditora</title>
+
+	<!-- Bootstrap
+    ================================================== -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="icons/font/bootstrap-icons.css">
+	
+	<!-- CSS
+    ================================================== -->
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+	<script src="jquery-3.7.1.js"></script>
+</head>
+<body>
+

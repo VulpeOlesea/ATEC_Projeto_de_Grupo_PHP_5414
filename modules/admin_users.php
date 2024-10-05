81443fe2536b3697dc2ad5f907ef2cd894ff09e4
VulpeOlesea <leseavulpe@gmail.com>
2024-10-05 20:54:37 +0100

    add admin_users

---
+++ modules/admin_users.php
@@ -0,0 +1,23 @@
+<div class="d-flex flex-row">
+	<div class="col-lg-12 col-sm-12">
+		<table>
+			<thead>
+				<th>Nome:</th>
+				<th>Sobrenome:</th>
+				<th>Email:</th>
+				<th>Distrito:</th>
+				<th>Concelho:</th>
+				<th>Perfil:</th>
+				<th></th>
+				<th></th>
+			</thead>
+			<tbody>
+				<?php function_list_users(); ?>
+			</tbody>
+		</table>
+
+	</div>
+</div>
+
+
+

f7473626dff7eae6ef6cacfde6efe21a1147e67f
VulpeOlesea <leseavulpe@gmail.com>
2024-10-19 18:03:28 +0100

    update

--- modules/header.php
+++ modules/header.php
@@ -16,15 +16,17 @@
 	<div class="flex-column col-lg-6 flex-end top-icons">
 		<ul>
 			<!-- Login - sim/não -->
-				<?php 
-				if(!$_SESSION["log_id"]){
-					echo '<li><a href="?nav=login"><i class="bi bi-person"></i> Conta</a></li>';
-				}else{
-					echo '<li><a href="#"><i class="bi bi-cart3"></i></a></li>';
-					echo '<li><a href="?nav=account"><i class="bi bi-person-fill"></i></a></li>';
-					echo '<li><a href="sair.php">Sair</a></li>';
-				}
-				?>
+			<?php 
+
+			if (isset($_SESSION['log_id'])) {
+				echo '<li><a href="#"><i class="bi bi-cart3"></i></a></li>';
+				echo '<li><a href="?nav=account"><i class="bi bi-person-fill"></i></a></li>';
+				echo '<li><a href="sair.php">Sair</a></li>';
+			} else {
+				echo '<li><a href="?nav=login"><i class="bi bi-person"></i> Conta</a></li>';
+			}
+			
+			?>
 		</ul>
 	</div>
 </header>

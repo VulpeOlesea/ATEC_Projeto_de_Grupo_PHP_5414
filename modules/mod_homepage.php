c2428c17a9eb252e41abc392cbbdc32c7066faeb
VulpeOlesea <leseavulpe@gmail.com>
2024-10-20 18:55:38 +0100

    add hompage

---
+++ modules/mod_homepage.php
@@ -0,0 +1,25 @@
+
+<?php function_section_banner(); ?>
+
+<section>
+	<section id="popular-books" class="bookshelf py-5 my-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+
+					<div class="section-header text-center">
+						<h2 class="section-title">Popular Books</h2>
+						
+					</div>
+
+					<div class="tab-content">
+						<div id="all-genre" data-tab-content="" class="active">
+							<?php function_section_products(); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</section>
+

bc9249355216af5eb606477fef039f70deaf8851
VulpeOlesea <leseavulpe@gmail.com>
2024-10-20 18:46:41 +0100

    add function products

--- functions/function_section_banner.php
+++ functions/function_section_banner.php
@@ -1,23 +1,15 @@
 <?php 
 
 function function_section_banner(){
-	include 'connections/config.php';
-	$q = mysqli_query($conn,"SELECT info_banner, info_title, info_description FROM info_general");
-	$a = mysqli_fetch_array($q);
-	echo '
-	<section>
-        <div class="d-flex flex-column col-lg-12 col-sm-12">
-            <div class="text-container">
-                <h3>'.$a["info_title"].'</h3>
-                <p>'.$a["info_description"].'</p>
-            </div>
-            <div class="image-container">
-                <img src="img/'.$a["info_banner"].'" style="width:100%; height:auto;">
-            </div>
+    include 'connections/config.php';
+    $q = mysqli_query($conn,"SELECT info_banner, info_title, info_description FROM info_general");
+    $a = mysqli_fetch_array($q);
+    echo '
+    <section class="banner-section" style="background-image: url(\'img/'.$a["info_banner"].'\'); background-size: cover; background-position: center; width: 100%; height: 100vh; position: relative;">
+        <div class="text-container" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center; color: white;">
+            <h3 style="font-size: 3em;">'.$a["info_title"].'</h3>
+            <p style="font-size: 1em;">'.$a["info_description"].'</p>
         </div>
     </section>';
-
 }
-
-
 ?>
---
+++ functions/function_section_products.php
@@ -0,0 +1,32 @@
+<?php 
+
+function function_section_products(){
+    include 'connections/config.php';
+    $q = mysqli_query($conn,"SELECT prod_image, prod_name, prod_author, prod_price FROM products");
+
+    if(mysqli_num_rows($q) > 0) {
+        echo '<div class="row">';
+        
+        while($a = mysqli_fetch_array($q)){
+            echo '
+            <div class="col-md-3">
+                <div class="product-item">
+                    <figure class="product-style">
+                        <img src="img/products/'.$a["prod_image"].'" alt="'.$a["prod_name"].'" class="product-thumb" style="width:100%; height:auto;">
+                        <button type="button" class="add-to-cart" data-product-tile="add-to-cart">Adicionar ao carrinho</button>
+                    </figure>
+                    <figcaption>
+                        <h3>'.$a["prod_name"].'</h3>
+                        <span>'.$a["prod_author"].'</span>
+                        <div class="item-price">€ '.$a["prod_price"].'</div>
+                    </figcaption>
+                </div>
+            </div>';
+        }
+
+        echo '</div>';
+    } else {
+        echo "<p>No products found.</p>";
+    }
+}
+?>

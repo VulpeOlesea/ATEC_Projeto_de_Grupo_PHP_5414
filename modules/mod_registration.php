325849693910c2426e1c915918c4b14a5de5fb35
VulpeOlesea <leseavulpe@gmail.com>
2024-10-21 11:48:14 +0100

    update margin

--- modules/mod_registration.php
+++ modules/mod_registration.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-lg-7">
-            <div class="card shadow-lg border-0 rounded-lg mt-5">
+            <div class="card shadow-lg border-0 rounded-lg mt-5 mb-5">
                 <div class="card-header"><h3 class="text-center font-weight-light my-4">Criar Conta</h3></div>
                     <div class="card-body">
                         <form method="post">

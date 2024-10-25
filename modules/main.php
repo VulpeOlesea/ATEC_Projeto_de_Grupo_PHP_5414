fb8477ae2c4633c8abc2fcc051147de9a081532c
VulpeOlesea <leseavulpe@gmail.com>
2024-10-25 09:55:23 +0100

    update main

--- modules/main.php
+++ modules/main.php
@@ -24,25 +24,3 @@
 
 	?>
 </main>
-
-
-<!-- Modal --
-<div class="modal" id="edicao_conta_utilizador" tabindex="-1" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-    	<form method="post">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5">Editar Utilizador</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body" id="formulario_edicao">
-        
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-        <button type="submit" class="btn btn-primary">Salvar</button>
-      </div>
-  		</form>
-    </div>
-  </div>
-</div> -->

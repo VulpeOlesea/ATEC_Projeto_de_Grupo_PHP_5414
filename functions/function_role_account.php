3190a1cf91c0e0a95743f547258d9d13abef80d4
VulpeOlesea <leseavulpe@gmail.com>
2024-10-25 19:25:25 +0100

    update

--- functions/function_role_account.php
+++ functions/function_role_account.php
@@ -1,7 +1,7 @@
 <?php 
 
 $log_id = $_REQUEST['log_id'];
-include 'connections/config.php';
+include '../connections/config.php';
 $q = mysqli_query($conn, "UPDATE login SET log_role = (log_role-1)* -1 WHERE log_id = '$log_id'");
 
 ?>

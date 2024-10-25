b9a5bad8b12fb4f05b70fa4ab2d5417c87b1e2df
VulpeOlesea <leseavulpe@gmail.com>
2024-10-25 10:45:25 +0100

    updade homepage

--- modules/mod_homepage.php
+++ modules/mod_homepage.php
@@ -2,13 +2,13 @@
 <?php function_section_banner(); ?>
 
 <section>
-	<section id="popular-books" class="bookshelf py-5 my-5">
+	<section id="popular-books" class="bookshelf py-5">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
 
-					<div class="section-header text-center">
-						<h2 class="section-title">Popular Books</h2>
+					<div class="section-header text-center pb-5">
+						<h2 class="section-title">As nossas publicações</h2>
 						
 					</div>
 

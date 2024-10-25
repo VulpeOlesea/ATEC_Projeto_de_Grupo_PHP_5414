3b329afb267883d9e37eaa8bf683c620576aaa4d
VulpeOlesea <leseavulpe@gmail.com>
2024-10-25 09:21:41 +0100

    +marg

--- functions/function_form_info_general.php
+++ functions/function_form_info_general.php
@@ -5,6 +5,7 @@ function function_form_info_general(){
 	$q = mysqli_fetch_array(mysqli_query($conn,"SELECT * FROM info_general WHERE info_id = 1"));
 	echo '
 	<div class="flex-column col-lg-6 col-sm-6">
+		<div style="width: 90%;">
 			<label>Telefone: </label>
 			<input type="text" name="info_telephone" class="form-control" value="'.$q["info_telephone"].'">
 			<label>Email: </label>
@@ -15,6 +16,7 @@ function function_form_info_general(){
 			<label>Descrição: </label>
 			<textarea name="info_description" class="form-control"> '.$q["info_description"].'</textarea>
 		</div>
+	</div>
 		<div class="flex-column col-lg-6 col-sm-6">
 			<img src="img/'.$q["info_banner"].'" style="width:50%; height:auto;"><br>
 			<label>Banner</label>
